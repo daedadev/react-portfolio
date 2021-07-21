eca30c7923a1ec2eb805e20ad35bb2df7a62e4ea
Daniel Moore <wron2@hotmail.com>
2021-07-20 21:53:05 -0700

    Added header html/css

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,2 +1,30 @@
 import React from "react";
 import "./style.css";
+
+function Header() {
+  return (
+    <header>
+      <div>
+        <h2>Daniel Moore</h2>
+        <img src="/Assets/images/profilepic.PNG" alt="My beautiful face" />
+      </div>
+
+      <nav>
+        <ul>
+          <li>
+            <a href="#about-me">About Me</a>
+          </li>
+          <li>
+            <a href="#work">Work</a>
+          </li>
+          <li>
+            <a href="#footer">Contact Me</a>
+          </li>
+          <li>
+            <a href="./resume.html">Resume</a>
+          </li>
+        </ul>
+      </nav>
+    </header>
+  );
+}
