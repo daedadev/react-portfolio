4327439c9244a91dcff050c5e3d420eae18a757a
Daniel Moore <wron2@hotmail.com>
2021-07-20 22:04:55 -0700

    Added footer html/css

---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,31 @@
+import React from "react";
+import "./style.css";
+
+function Footer() {
+  return (
+    <footer id="footer" class="left-and-right">
+      <article class="left">
+        <h1>Contact</h1>
+        <h1>Me</h1>
+      </article>
+      <article class="right">
+        <nav>
+          <ul>
+            <li>
+              <p>951-427-8876</p>
+            </li>
+            <li>
+              <p>wron2@hotmail.com</p>
+            </li>
+            <li>
+              <a href="https://github.com/wron1">Github</a>
+            </li>
+            <li>
+              <a href="https://www.instagram.com/wron1.kicks/">Instagram</a>
+            </li>
+          </ul>
+        </nav>
+      </article>
+    </footer>
+  );
+}
--- src/components/Wrapper/index.js
+++ src/components/Wrapper/index.js
@@ -1,2 +1,4 @@
 import React from "react";
 import "./style.css";
+
+function Wrapper() {}
