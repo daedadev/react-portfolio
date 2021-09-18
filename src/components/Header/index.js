68546587dbb0f6a24d48b776658d487c1f777014
Daniel Moore <wron2@hotmail.com>
2021-09-17 19:25:06 -0700

    Added a menu dropdown for mobile display

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -7,8 +7,8 @@ export default class Header extends React.Component {
     theClassName: "default",
   };
 
-  listenScrollEvent = (e) => {
-    if (window.scrollY > 100) {
+  activateDirectory = () => {
+    if (this.state.theClassName === "default") {
       this.setState({
         theClassName: "modified",
       });
@@ -17,21 +17,44 @@ export default class Header extends React.Component {
     }
   };
 
-  componentDidMount() {
-    window.addEventListener("scroll", this.listenScrollEvent);
-  }
-
   render() {
     return (
-      <header className={this.state.theClassName}>
+      <header>
         <section className="header-top">
-          <div className={this.state.theClassName}>
+          <div>
             <img
               src={`${process.env.PUBLIC_URL + "/images/profilepic.PNG"}`}
               alt="My beautiful face"
             />
             <h2>Daniel Moore</h2>
           </div>
+
+          <button className="menu-button">
+            <h3 onClick={this.activateDirectory}>Menu</h3>
+          </button>
+
+          <nav className="header-nav">
+            <ul>
+              <li>
+                <a style={{ color: this.state.textColor }} href="#about-me">
+                  About Me
+                </a>
+              </li>
+              <li>
+                <a style={{ color: this.state.textColor }} href="#work">
+                  Work
+                </a>
+              </li>
+              <li>
+                <a style={{ color: this.state.textColor }} href="#footer">
+                  Contact Me
+                </a>
+              </li>
+              <li>
+                <Link to={"/resume"}>Resume</Link>
+              </li>
+            </ul>
+          </nav>
 
           <nav className={this.state.theClassName}>
             <ul>
