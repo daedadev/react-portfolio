c8321a1712c1e3b6b24730241e4eb566ca5db53a
Daniel Moore <wron2@hotmail.com>
2021-12-07 01:23:30 -0800

    Added resume component

---
+++ src/components/Resume/index.js
@@ -0,0 +1,101 @@
+import React from "react";
+import "./style.css";
+
+function Resume() {
+  return (
+    <div>
+      <h1>Daniel Moore</h1>
+      <br />
+      <h2>
+        8368 Daisy Lane, Riverside CA 92508 | (951)427-8876 | wron2@hotmail.com{" "}
+      </h2>
+      <br />
+      <h1>Education</h1>
+      <br />
+      <p>HIGH SCHOOL GRADUATE 2011-2015 </p>
+      <p>High School Diploma from Martin Luther King Jr. High School </p>
+      <br />
+      <h1>BM | CALIFORNIA STATE UNIVERSITY FULLERTON 2015 - 2020</h1>
+      <p>Major: Music Performance</p>
+      <p>
+        Related coursework: Essays, a lot of essays. Documenting musical eras,
+        and 15-page research papers.{" "}
+      </p>
+      <br />
+      <h1>CERTIFICATE | UNIVERSITY CALIFORNIA RIVERSIDE 2021-2021 </h1>
+      <p>Field of Study: Full Stack Web Development </p>
+      <p>Related coursework: Working with HTML, CSS, MySQL, REACT </p>
+      <p>UCR Extension: Coding Bootcamp </p>
+      <br />
+      <h1>Skills & Abilities </h1>
+      <br />
+      <h1>COMMUNICATION </h1>
+      <p>
+        Going through school requires the skill of communication to present many
+        projects and speeches in front of peers and strangers and working in
+        food service requires a wide variety of conversational skills. From
+        handling upset customers to dealing with stagnant co-workers I have been
+        there and done that. 
+      </p>
+      <br />
+      <h1>Hard Work</h1>
+      <p>
+        Hard work was taught to me since I was a 13-year-old boy by my oboe
+        instructor, and continue to be a part of my character to this day. It
+        has persisted with me to always do my best and keep pushing forward even
+        when nobody is looking. Never cut corners and always give 100%.
+      </p>
+      <br />
+      <h1>Leadership</h1>
+      <p>
+        I was in the band for all 4 years at Martin Luther King High School,
+        with 2 of those years being in the leadership program, and my senior
+        year being the leader of my section. In customer service I was the shift
+        lead for a majority of my time and training new employees was a part of
+        my job.
+      </p>
+      <br />
+      <h1>Experience</h1>
+      <br />
+      <h1>OFFICE ASSISTANT | RIVERSIDE COUNTY PHILHARMONIC | 2018 - 2020 </h1>
+      <p>
+        I worked with the Riverside County Philharmonic for 2 years helping them
+        with a various amount of their day to day needs as an organization.{" "}
+      </p>
+      <p>
+        A majority of my time was dedicated to doing office work for the
+        Executive Director at the time. I would proofread documents and print
+        out fliers.
+      </p>
+      <p>
+        Long work hours are not a problem for me as many days I would be working
+        for 10 or more hours at a time.{" "}
+      </p>
+      <br />
+      <h1>FOOD INDUSTRY</h1>
+      <p>I worked at Dickey’s Barbecue in the Riverside Plaza for 8 months.</p>
+      <p>
+        I was the front-man Block/shift lead who would take orders and prepare
+        food. I would also ensure that everything was going smoothly in the
+        restaurant while the manager was away.
+      </p>
+      <p>
+        Training new employees and generally having knowledge of every job was
+        necessary to keep the restaurant running at full capacity.{" "}
+      </p>
+      <br />
+      <h1>PROGRAMMING </h1>
+      <p>
+        I have a few comfort projects which started me into programming. They
+        use C# and unity to create fun mobile games which can be found on my
+        itch.io page. https://weapontostopwar.itch.io/{" "}
+      </p>
+      <p>
+        I also have a lot of projects that I have made over the course of my
+        bootcamp that can be found on my GitHub. https://github.com/wron1{" "}
+      </p>
+    </div>
+  );
+}
+
+export default Resume;
