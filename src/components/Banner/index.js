3fdb33f0a08a8a1f3103424bba43eb71968dbf29
Daniel Moore <wron2@hotmail.com>
2022-01-27 09:53:08 -0800

    Updated image sizing on mobile

--- src/components/Banner/index.js
+++ src/components/Banner/index.js
@@ -6,10 +6,10 @@ function Banner() {
     <div className="banner">
       <h1>Javascript</h1>
       <h1>HTML</h1>
-      <h1>MongoDB</h1>
-      <h1>mySQL</h1>
+      <h1 className="theOthers">MongoDB</h1>
+      <h1 className="theOthers">mySQL</h1>
       <h1>React</h1>
-      <h1>Handlebars</h1>
+      <h1 className="theOthers">Handlebars</h1>
     </div>
   );
 }
