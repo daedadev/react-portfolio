5f11b57fcc5efa47c55b42077e905fcddf27911f
Daniel Moore <wron2@hotmail.com>
2022-01-29 17:57:56 -0800

    Updated bio

--- src/components/AboutMe/index.js
+++ src/components/AboutMe/index.js
@@ -12,11 +12,10 @@ function AboutMe() {
         <p>
           My name is Daniel Moore and I am a 24 year old programmer who
           specifies in React and Javascript coding. I have studied Javascript
-          and CSS under the direction of Farish Kashefinejad while at the
-          University of Riverside Coding Bootcamp. I have a decent amount of
-          previous experience with coding through a couple years of using Unity
-          to develop video games for mobile devices. I also have experience
-          working with google chrome extensions and have made a few self
+          and CSS under the direction of Farish Kashefinejad at the University
+          of Riverside Coding Bootcamp. I have experience with coding through 2
+          years of using Unity to develop video games for mobile devices. I also
+          have experience working with google chrome extensions and made
           projects using electron.
         </p>
 
@@ -47,21 +46,18 @@ function AboutMe() {
         </section>
 
         <p>
-          Since starting the coding bootcamp at UCR I have gained much knowledge
-          involving the use of MySQL, mongoDB, local web storage and utilizing
-          and creating API's. I have learned so much during the course of the
-          camp, team work and fast learning being amongst my most polished
-          skills. The larger sized units of the projects below are some
-          displaying that knowledge while the smaller works are some of my
-          personal accomplishments.
+          Throughout UCR's coding bootcamp, I have acquired working knowledge of
+          MySQL mongoDB and local web storage, alongside utilizing and creating
+          API's. Amongst my most polished skills are team work and fast
+          learning. The larger projects below are displaying that knowledge
+          while the smaller works are some of my personal accomplishments.
         </p>
 
         <p>
-          After completing the bootcamp amongst the top of my class I have come
-          to know quickly that I have a lot to learn and am developing my skills
-          day after day. My team work skills, quick learning and highly flexible
-          personality make me an adaptable worker and an amazing asset to any
-          team. Thank you for giving this a read and I hope that you enjoy.
+          After completing the bootcamp amongst the top of my class I am eager
+          to learn and contribute my skills day after. My adaptability and
+          flexible personality make me invaluable to any team. Thank you for
+          your time and contact me down below with any questions.
         </p>
       </article>
     </section>
