86a8bc634a2e2ebea4ab1a36c806b5587cb01673
Daniel Moore <wron2@hotmail.com>
2022-02-05 23:21:29 -0800

    Updated landing page

--- src/components/Banner/index.js
+++ src/components/Banner/index.js
@@ -6,20 +6,24 @@ function Banner() {
   return (
     <div className="banner">
       <div id="text-holder">
-        <h1>My Name is Daniel Moore</h1>
+        <h1>
+          My Name is <strong>Daniel</strong>
+        </h1>
         <div id="carousel-holder">
           <h3 id="text-1">I am a </h3>
           <Carousel
             enableAutoPlay
-            autoPlaySpeed={2000}
+            autoPlaySpeed={1500}
             verticalMode
             itemsToShow={1}
           >
             <h3 className="text-mid">Front End</h3>
             <h3 className="text-mid">Back End</h3>
+            <h3 className="text-mid">Front End</h3>
+            <h3 className="text-mid">Back End</h3>
             <h3 className="text-mid">Full Stack</h3>
           </Carousel>
-          <Carousel enableAutoPlay autoPlaySpeed={2000} itemsToShow={1}>
+          <Carousel enableAutoPlay autoPlaySpeed={3000} itemsToShow={1}>
             <h3 className="text-bottom">Creator</h3>
             <h3 className="text-bottom">Thinker</h3>
             <h3 className="text-bottom">Web Developer</h3>
