843f8e9c4dacea888555506f19910639a034a576
daedadev <danielchrismoore@gmail.com>
2022-10-30 20:22:21 -0700

    Adding hashrouter

--- src/components/AppNav/index.tsx
+++ src/components/AppNav/index.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import "./style.css";
-import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import { HashRouter as Router, Switch, Route } from "react-router-dom";
 import { MainPage } from "../../pages/MainPage";
 import { Wrapper } from "../Wrapper";
 import { Header } from "../Header";
@@ -9,11 +9,20 @@ import ResumePage from "../../pages/ResumePage";
 
 export function AppNav() {
   return (
-    <Router>
+    <Router baseline='/'>
       <Wrapper>
         <Header />
         <Switch>
           <Route exact path="/">
+            <MainPage />
+          </Route>
+          <Route exact path="/aboutme-section">
+            <MainPage />
+          </Route>
+          <Route exact path="/work-section">
+            <MainPage />
+          </Route>
+          <Route exact path="/footer">
             <MainPage />
           </Route>
           <Route exact path="/resume">
