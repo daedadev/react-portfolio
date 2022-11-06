e4a3daaa867938feba3decfb453ca276fb2caa70
daedadev <danielchrismoore@gmail.com>
2022-11-05 18:22:21 -0700

    Fixing scroll to sections to adjust segmented page height

--- src/components/LargeContainer/index.tsx
+++ src/components/LargeContainer/index.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect } from "react";
 import "./style.css";
 import { AboutMe } from "../AboutMe";
 import { Project } from "../Project";
--- src/components/ProjectModal/index.tsx
+++ src/components/ProjectModal/index.tsx
@@ -31,7 +31,7 @@ export function ProjectModal({
     return () => {
       window.removeEventListener("click", handleWindowClick);
     };
-  }, [toggleModal]);
+  }, [toggleModal, handleWindowClick]);
 
   return (
     <>
